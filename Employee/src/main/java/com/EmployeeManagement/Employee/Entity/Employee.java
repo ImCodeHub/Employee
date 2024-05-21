d786fa764a7dd35440c01e76677028d71850cd60
Ankit <ankitsharma.as420@gmail.com>
2024-05-21 23:35:49 +0530

    created Entity for Employee

---
+++ Employee/src/main/java/com/EmployeeManagement/Employee/Entity/Employee.java
@@ -0,0 +1,29 @@
+package com.EmployeeManagement.Employee.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name="first_name")
+    private String firstName;
+    
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email", unique = true)
+    private String email;
+
+    private String department;
+
+}
+
