42a2f3e4a958079de7064b2489d0f1be3d35d459
Ankit <ankitsharma.as420@gmail.com>
2024-05-25 09:31:32 +0530

    update table name and column name

--- Employee/src/main/java/com/EmployeeManagement/Employee/Entity/Employee.java
+++ Employee/src/main/java/com/EmployeeManagement/Employee/Entity/Employee.java
@@ -5,24 +5,25 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "employee")
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="first_name")
+    @Column(name = "first_name")
     private String firstName;
-    
-    @Column(name="last_name")
+
+    @Column(name = "last_name")
     private String lastName;
 
-    @Column(name="email", unique = true)
+    @Column(name = "email", unique = true)
     private String email;
 
     private String department;
 }
-
