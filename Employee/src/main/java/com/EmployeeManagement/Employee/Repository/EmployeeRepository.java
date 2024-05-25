129d49aa7727accd5528a6a35b147088f229b4a4
Ankit <ankitsharma.as420@gmail.com>
2024-05-25 09:33:16 +0530

    create method to findByEmail

--- Employee/src/main/java/com/EmployeeManagement/Employee/Repository/EmployeeRepository.java
+++ Employee/src/main/java/com/EmployeeManagement/Employee/Repository/EmployeeRepository.java
@@ -1,5 +1,8 @@
 package com.EmployeeManagement.Employee.Repository;
 
+
+import java.util.*;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,5 @@ import com.EmployeeManagement.Employee.Entity.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    
+    Optional<Employee> findByEmail(String email);
 }
